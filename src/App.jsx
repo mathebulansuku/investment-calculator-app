c118d8ccc09c7959a9fe1a25197335c98311fc0b
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-07 16:49:41 +0200

    Investment Calculator App complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,13 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
@@ -25,7 +27,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChangeInput={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && <p className="center">Please enter a valid duration</p>}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
