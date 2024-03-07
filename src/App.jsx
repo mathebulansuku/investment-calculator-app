7185f7e146cf74dbc7c127e356b5c6843dc5f168
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-07 15:26:47 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components.jsx/Header";
+import { calculateInvestmentResults } from "./util/investment";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
